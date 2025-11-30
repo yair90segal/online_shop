60e8069826d5bf99c042961146434ffc4d01d276
Student SKZ901 <tchstud2025y2901@gmail.com>
2025-11-30 15:12:41 +0200

    Feat(SearchResults.tsx): presents search results page

--- frontend/src/pages/SearchResults.tsx
+++ frontend/src/pages/SearchResults.tsx
@@ -1,18 +1,35 @@
-import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom"
+import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useProducts } from "../hooks/useProduct";
+import ProductCard from "../components/ProductCard/ProductCard";
+import type { ProductDetails } from "../types/productDetails";
 
 const SearchResults = () => {
-    const [searchParams] = useSearchParams();
-    const query = searchParams.get("query") ?? "";
-    // product need to be context
-    // add result variable, reset to empty list. in the use effect we will assign value of filtered product list
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query")?.trim() ?? "";
+  const { products } = useProducts();
+  // product need to be context
+  // add result variable, reset to empty list. in the use effect we will assign value of filtered product list
 
-    useEffect(() => {
-        if (!query) {
-            return;
-        }
+  const results = useMemo(() => {
+    if (!query) {
+      return products;
+    }
 
-    }, [query]);
-}
+    return products.filter(
+      (product) =>
+        product.productName.toLowerCase().includes(query.toLowerCase()) ||
+        product.author.toLowerCase().includes(query.toLowerCase())
+    );
+  }, [query, products]);
+
+  return (
+    <div>
+      {results.map((product: ProductDetails) => (
+        <ProductCard key={product.productId} product={product} />
+      ))}
+    </div>
+  );
+};
 
 export default SearchResults;
