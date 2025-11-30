4708a8888fd15819377cb97970f637f99e4daf65
Student SKZ901 <tchstud2025y2901@gmail.com>
2025-11-30 19:59:09 +0200

    Feat(app.module.ts, datasource.ts): add connection to db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import datasource from './datasource';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        ...datasource.options,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
