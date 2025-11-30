c3c204f5461737deac94fd10b882b9ef83f222bc
Student SKZ901 <tchstud2025y2901@gmail.com>
2025-11-30 11:53:09 +0200

    Feat(MainLayout.tsx): wrap main layout with ProductProvider

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar/Navbar";
 import { useState } from "react";
+import { ProductProvider } from "../contexts/ProductProvider";
 
 const MainLayout = () => {
-    const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("");
 
-    return (
+  return (
     <div style={{ minHeight: "100vh" }}>
-      <Navbar isAdmin={false} search={search} setSearch={setSearch}/>
-      <div style={{ paddingTop: "64px" }}>
-        <Outlet />
-      </div>
+      <ProductProvider>
+        <Navbar isAdmin={false} search={search} setSearch={setSearch} />
+        <div style={{ paddingTop: "64px" }}>
+          <Outlet />
+        </div>
+      </ProductProvider>
     </div>
   );
 };
