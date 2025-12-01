1108ebc39c9cba6f6c214eed48b4128ea6293dc8
Student SKZ901 <tchstud2025y2901@gmail.com>
2025-12-01 17:31:39 +0200

    Feat(user.service.ts): check if user exist before saving to db, created helper functions for that

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserLocalDto } from './dto/createUserLocal.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { Repository } from 'typeorm';
+
+export interface UserResponse {
+  userId: number;
+  token: string;
+}
 
 @Injectable()
 export class UserService {
@@ -10,12 +15,39 @@ export class UserService {
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
   ) {}
+
   async createUser(
     createUserDto: CreateUserLocalDto,
   ): Promise<CreateUserLocalDto> {
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
 
+    const userByEmail = await this.findByEmail(newUser.email);
+    const userByUsername = await this.findByUsername(newUser.username);
+
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        'Email or username already exist',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
     return await this.userRepository.save(newUser);
+  }
+
+  async findByUsername(username: string) {
+    return await this.userRepository.findOne({
+      where: {
+        username: username,
+      },
+    });
+  }
+
+  async findByEmail(email: string) {
+    return await this.userRepository.findOne({
+      where: {
+        email: email,
+      },
+    });
   }
 }
