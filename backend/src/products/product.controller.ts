a4e00dbf5a7aefc195b5dc3344a312a4fab802e6
Student SKZ901 <tchstud2025y2901@gmail.com>
2025-12-03 18:35:39 +0200

    Feat(product.controller.ts): implemented product controller routes

--- backend/src/products/product.controller.ts
+++ backend/src/products/product.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateProductDto } from './dto/createProductDto';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  async getAllProducts() {
+    return await this.productService.getAllProducts();
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor('image'))
+  async createProduct(
+    @Body() dto: CreateProductDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return await this.productService.createProduct(dto, file);
+  }
+
+  @Delete(':id')
+  async deleteProduct(@Param('id') id: string) {
+    return await this.productService.removeProduct(id);
+  }
 }
